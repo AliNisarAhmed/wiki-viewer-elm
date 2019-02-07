195f84ad9cb3f0de507d6374fd5a88dbbb4964d2
AliNisarAhmed <ali.nisar87@gmail.com>
2019-02-07 21:37:48 +0500

    final

--- src/Main.elm
+++ src/Main.elm
@@ -4,8 +4,11 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Html.Parser
+import Html.Parser.Util
 import Http
 import Json.Decode as D exposing (..)
+import Url exposing (percentEncode)
 
 
 
@@ -16,17 +19,27 @@ type alias Model =
     { formActive : Bool
     , searchField : String
     , searchResults : List ResponseObject
+    , isLoading : Bool
     }
 
 
 wikiUrl : String
 wikiUrl =
-    "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=$react"
+    "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch="
+
+
+initModel : Model
+initModel =
+    { formActive = False
+    , searchField = ""
+    , searchResults = []
+    , isLoading = False
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { formActive = False, searchField = "", searchResults = [] }, Cmd.none )
+    ( initModel, Cmd.none )
 
 
 
@@ -44,18 +57,31 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         ToggleActive ->
-            ( { model | formActive = not model.formActive }, Cmd.none )
+            let
+                field =
+                    case model.formActive of
+                        False ->
+                            model.searchField
+
+                        True ->
+                            ""
+            in
+            ( { model | formActive = not model.formActive, searchField = field }, Cmd.none )
 
         SearchInput input ->
             ( { model | searchField = input }, Cmd.none )
 
         FormSubmit ->
-            ( { model | searchField = "" }, getResponseFromWiki )
+            let
+                searchBy =
+                    model.searchField
+            in
+            ( { model | searchField = "", isLoading = True }, getResponseFromWiki searchBy )
 
         Response result ->
             case result of
                 Ok res ->
-                    ( { model | searchResults = res }, Cmd.none )
+                    ( { model | searchResults = res, isLoading = False }, Cmd.none )
 
                 Err _ ->
                     ( { model | searchResults = [ { title = "error", pageid = 0, snippet = "" } ] }, Cmd.none )
@@ -79,18 +105,29 @@ view model =
                 , text " to read a random article"
                 ]
             , Html.form [ class (toggleForm model.formActive), onSubmit FormSubmit ]
-                [ input [ class "input", onFocus ToggleActive, onBlur ToggleActive, onInput SearchInput ] [] ]
+                [ input [ Html.Attributes.value model.searchField, class "input", onFocus ToggleActive, onBlur ToggleActive, onInput SearchInput, required True ] [] ]
             , p [] [ text "click the icon to search" ]
             ]
-        , div [ class "result-list" ] (List.map displaySearchResults model.searchResults)
+        , displayLoadingIconOrSearchResults model
         ]
+
+
+displayLoadingIconOrSearchResults : Model -> Html Msg
+displayLoadingIconOrSearchResults model =
+    case model.isLoading of
+        True ->
+            div [ class "loading-icon-parent" ]
+                [ div [ class "loading-icon" ] [] ]
+
+        False ->
+            div [ class "result-list" ] (List.map displaySearchResults model.searchResults)
 
 
 displaySearchResults : ResponseObject -> Html Msg
 displaySearchResults listItem =
-    a [ class "result" ]
+    a [ class "result", href ("https://en.wikipedia.org/wiki?curid=" ++ String.fromInt listItem.pageid), target "_blank" ]
         [ h3 [ class "result-title" ] [ text listItem.title ]
-        , p [] [ text listItem.snippet ]
+        , div [] (displaySnippet listItem.snippet)
         ]
 
 
@@ -102,6 +139,16 @@ toggleForm x =
 
         _ ->
             ""
+
+
+displaySnippet : String -> List (Html Msg)
+displaySnippet str =
+    case Html.Parser.run str of
+        Ok res ->
+            Html.Parser.Util.toVirtualDom res
+
+        Err _ ->
+            [ p [] [ text "something went wrong parsing the snippet" ] ]
 
 
 
@@ -122,9 +169,9 @@ main =
 -- Http
 
 
-getResponseFromWiki : Cmd Msg
-getResponseFromWiki =
-    Http.get { url = wikiUrl, expect = Http.expectJson Response responseDecoder }
+getResponseFromWiki : String -> Cmd Msg
+getResponseFromWiki searchField =
+    Http.get { url = wikiUrl ++ percentEncode searchField, expect = Http.expectJson Response responseDecoder }
 
 
 type alias ResponseObject =
